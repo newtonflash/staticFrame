97da10d6e64531f4c4d81582e0b78c17403d09af
SHOURYA RANKA <sranka.2310@gmail.com>
2015-06-17 10:39:15 +0530

    Update ns.global.js

--- public/js/ns.global.js
+++ public/js/ns.global.js
@@ -1,43 +1,51 @@
 /**
- *	global.js
- * 	Global footer file
- *
- *  @project    < write project name here >
- *  @date       < put date here > 
- *  @author  	< author name >	
- * 	@site       < site name >
- */
+*             global.js
+*   Global footer file
+*
+*   @project    < write project name here >
+*   @date       < put date here > 
+*   @author     < author name >  
+*   @site       < site name >
+*/
 
- (function($, ns, window, document, undefined){
-	
-	"use strict";
+(function ($, ns, window, document, undefined) {
+                
+                "use strict";
+    
+                var Global = function () {
+                                /**
+                                *            add all event listeners
+                                */
+                                var initEventListeners = function () {
+                                                
+                                };
+        
+                                /**
+                                *            put all event triggers, plugin initializations
+                                */
+                                var initEventTriggers = function () {
 
-	var _global = function(){
-		this.init = function(){
-			initEventListeners();
-			initEventTriggers();
-		};
+                                };
+        
+        /**
+         *  define what needs to be done after all stuff 
+         *  has been loaded in to the browser
+         **/
+        this.onLoad = function () {
+        };
+        
+        this.init = function () {
+                                                initEventListeners();
+                                                initEventTriggers();
+                                };
+                                /**
+                                * Initialize this object when document ready event is triggered
+                                */
+                                $.subscribe(ns.events.INIT_MODULES, this.init);
+        $.subscribe(ns.events.WINDOW_LOAD, this.onLoad);
+        
+                };
 
-		/**
-		 *	add all event listeners
-		 */
-		var initEventListeners = function(){
-			
-		};
-		
-		/**
-		 *	put all event triggers, plugin initializations
-		 */
-		var initEventTriggers = function(){
+                ns.global = new Global();
 
-		};
-
-		/**
-		 * Initialize this object when document ready event is triggered
-		 */
-		$.subscribe(ns.events.INIT_MODULES, this.init);
-	};
-
-	ns.global = new _global();
-
-})((typeof jQuery !== "undefined") ? jQuery : null, NameSpace || {}, window, window.document);
+})((typeof window.jQuery !== "undefined") ? window.jQuery : null, window.NameSpace || {}, window, window.document);
