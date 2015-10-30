126e3bf1843911d68658e09ce786c27244a2681a
Pradyumna Ghose <pghose@sapient.com>
2015-10-30 10:46:58 +0530

    updated favicon

--- public/js/sf.preinit.js
+++ public/js/sf.preinit.js
@@ -8,53 +8,56 @@ var SF = SF || {};
 SF.cfg = SF.cfg || {};
 SF.events = SF.events || {};
 
-(function ($, ns, window, document, undefined){
+(function ($, sf, window, document, undefined){
 	(function(){
 		var ua = navigator.userAgent,
 			isDevEnv = (location.host.indexOf("localhost") !== -1)? 1 : -1 ,
 			html = document.getElementsByTagName("html")[0];
-		
-		ns.cfg = ns.cfg || {};
-		ns.cfg.isIOS = /iPad/i.test(ua) || /iPhone/i.test(ua);
-		ns.cfg.isAndroid = /Android/i.test(ua);
-		ns.cfg.isIE = /MSIE (\d+\.\d+);/.test(ua)  || /Trident\/7\./.test(ua);
-		ns.cfg.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(ua);
-		ns.cfg.isTouchEnabled = (function is_touch_device() {
-									 return (('ontouchstart' in window)
-										  || (navigator.MaxTouchPoints > 0)
-										  || (navigator.msMaxTouchPoints > 0));
-									})();
-		ns.cfg.isAndroid && html.classList.add("android");
-		!ns.cfg.isMobile &&	html.classList.add("no-touch");
-		
-		ns.cfg.breakPoints ={
-            MOBILE_ONLY             :"screen and (min-width:0px) and (max-width:767px)",
-			MOBILE_PORTRAIT_ONLY    :"screen and (min-width:0px) and (max-width:480px)",
-			MOBILE_LANDSCAPE_ONLY   :"screen and (min-width:481px) and (max-width:767px)",
-            MOBILE_TABLET_ONLY      :"screen and (min-width:0px) and (max-width:1024px)",
-			TABLET_ONLY             :"screen and (min-width:768px) and (max-width:1024px)",
-			DESKTOP_ONLY            :"screen and (min-width:1025px) and (max-width:1280px)",
-			DESKTOP_CINEMA          :"screen and (min-width:1025px)",
-            CINEMA                  :"screen and (min-width:1281px)"
+
+		sf.events = sf.events || {};
+		sf.cfg = sf.cfg || {};
+		sf.cfg.isIOS = /iPad/i.test(ua) || /iPhone/i.test(ua);
+		sf.cfg.isAndroid = /Android/i.test(ua);
+		sf.cfg.isIE = /MSIE (\d+\.\d+);/.test(ua) || /Trident\/7\./.test(ua);
+		sf.cfg.isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(ua);
+		sf.cfg.isTouchEnabled = (function is_touch_device() {
+			return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+		})();
+
+		if(ua.match(/Windows Phone/i)){
+			html.classList.add("win-phone");
 		}
 
-		ns.events = {
-			WINDOW_RESIZE : "asx/screen/resize/",
-			WINDOW_LOAD : "asx/window/load/",
-			SCROLL : "asx/mouse/scroll",
-			INIT_MODULES : "asx/modules/init/",
-
-            VIEWPORT_CHANGE : "asx/viewport/change/", //for responsive implementation
-            VIEWPORT_MOBILE_ONLY:"viewport/mobileonly/",
-			VIEWPORT_MOBILE_PORTRAIT_ONLY:"viewport/mobileportrait/",
-			VIEWPORT_MOBILE_LANDSCAPE_ONLY:"viewport/mobilelandscape/",
-            VIEWPORT_MOBILE_TABLET_ONLY:"viewport/mobileAndTablet/",
-			VIEWPORT_TABLET_ONLY:"viewport/tablet only/",
-            VIEWPORT_DESKTOP_ONLY:"viewport/desktop only/",
-			VIEWPORT_DESKTOP_CINEMA:"viewport/desktopAndAbove/",
-            VIEWPORT_CINEMA :"viewport/large cinema display/"
+		if(ua.match(/iemobile/i)){
+			html.classList.add("ie-mobile");
 		}
-		ns.cfg.viewport = ns.events.VIEWPORT_CINEMA;
+
+		if (sf.cfg.isAndroid) html.classList.add("android");
+		if (!sf.cfg.isMobile) html.classList.add("no-touch");
+		if (sf.cfg.isIE) html.classList.add("ie");
+		
+		sf.cfg.breakPoints ={
+			XSVP            :"screen and (min-width:0px) and (max-width:480px)",
+            SVP             :"screen and (min-width:480px) and (max-width:768px)",
+			MVP				:"screen and (min-width:769px) and (max-width:1024px)",
+			LVP				:"screen and (min-width:1025px) and (max-width:1280px)",
+			XLVP			:"screen and (min-width:1281px)"
+		}
+
+		sf.events = {
+			WINDOW_RESIZE : "sf/resize",
+			WINDOW_LOAD : "sf/load",
+			SCROLL : "sf/scroll",
+			INIT_MODULES : "sf/init/",
+
+            VIEWPORT_CHANGE : "sf/viewport/change/",
+            VIEWPORT_XSVP:"sf/viewport/extraSmall",
+            VIEWPORT_SVP:"sf/viewport/small",
+            VIEWPORT_MVP:"sf/viewport/medium",
+            VIEWPORT_LVP:"sf/viewport/large",
+            VIEWPORT_XLVP:"sf/viewport/extraLarge"
+		}
+		sf.cfg.viewport = sf.events.VIEWPORT_XLVP;
 
 	})();
 
@@ -64,50 +67,51 @@ SF.events = SF.events || {};
 	 *  3. Setting up mediaQuery breakpoints and adding eventlisteners to trigger publishing VIEWPORT_CHANGE EVENT
 	 **/
 	$(document).ready(function(){
-        var uniqueBreakPointRegex = /\bMOBILE_PORTRAIT_ONLY\b|\bTABLET_ONLY\b|\bCINEMA\b|\bMOBILE_LANDSCAPE_ONLY\b|\bDESKTOP_ONLY\b/;
+        var uniqueBreakPointRegex = /\bXSVP\b|\bSVP\b|\bMVP\b|\bLVP\b|\bXLVP\b/;
 
-		ns.root = $('body'); // this is for caching body for faster search inside it.
+		sf.root = $('body'); // this is for caching body for faster search inside it.
 
 		// avoid subscribing to WINDOW_RESIZE event where ever possible
 		$(window).resize($.throttle( 250, function(){
-			$.publish(ns.events.WINDOW_RESIZE);
+			$.publish(sf.events.WINDOW_RESIZE);
 		}));
 
-		if( ns.cfg.isIE ){
+		if( sf.cfg.isIE ){
 			$(window).scroll( $.throttle( 500, function(){
-				$.publish(ns.events.SCROLL);
+				$.publish(sf.events.SCROLL);
 			}));
 		} else{
 			$(window).scroll( $.throttle( 20, function(){
-				$.publish(ns.events.SCROLL);
+				$.publish(sf.events.SCROLL);
 			}));
 		}
 		// for first time before module initialization
-		for (breakPoint in ns.cfg.breakPoints) {
-			mediaQuery = ns.cfg.breakPoints[breakPoint];
+		for (breakPoint in sf.cfg.breakPoints) {
+			mediaQuery = sf.cfg.breakPoints[breakPoint];
 			if (matchMedia(mediaQuery).matches) {
 				if (uniqueBreakPointRegex.test(breakPoint)) {
-					ns.cfg.viewport = breakPoint;
+					sf.cfg.viewport = breakPoint;
 				}
 			}
 		}
 
-		$.publish(ns.events.INIT_MODULES);
+		$.publish(sf.events.INIT_MODULES);
 
 		// publish events on viewport change based on media queries, after document ready
-		for ( var breakPoint in  ns.cfg.breakPoints){
-			;(function(breakName, mediaQuery){
+		for ( var breakPoint in  sf.cfg.breakPoints){
+
+			(function(breakName, mediaQuery){
 				var handler = function(data){
 					if(data.matches){
 						var vpAlias = "VIEWPORT_"+breakName;
-						$.publish(ns.events[vpAlias], true);
+						$.publish(sf.events[vpAlias], true);
 
 						if(uniqueBreakPointRegex.test(breakName)) {
-							ns.cfg.viewport = breakName;
-							$.publish(ns.events.VIEWPORT_CHANGE);
+							sf.cfg.viewport = breakName;
+							$.publish(sf.events.VIEWPORT_CHANGE);
 						}
 					} else {
-						$.publish(ns.events["VIEWPORT_"+breakName], false);
+						$.publish(sf.events["VIEWPORT_"+breakName], false);
 					}
 				};
 				handler({
@@ -116,12 +120,12 @@ SF.events = SF.events || {};
 				});
 				window.matchMedia(mediaQuery).addListener(handler);
 
-			})(breakPoint, ns.cfg.breakPoints[breakPoint]);
+			})(breakPoint, sf.cfg.breakPoints[breakPoint]);
 		};
 
 	});
 	$(window).load(function(){
-		$.publish(ns.events.WINDOW_LOAD);
+		$.publish(sf.events.WINDOW_LOAD);
 	});
 })(jQuery, SF, this, this.document);
 
